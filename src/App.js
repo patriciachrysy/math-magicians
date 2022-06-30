725cc83792854a20a8efb4ef633da9de2bcfc2bb
Manezeu Patricia <pmanezeu@gmail.com>
2022-06-30 05:54:44 +0100

    Turned App into a class based component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render = () => <Calculator />;
 }
 
 export default App;
